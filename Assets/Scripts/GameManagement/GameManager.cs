28ce4e90867eebea300003ac325161afe540dc09
Bruno Croci <crocidb@gmail.com>
2020-01-11 20:34:08 +0000

    adding game manager and creating basic main menu

---
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GameManagement
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake() 
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
