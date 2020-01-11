efbae10dc72b738496e73446b3581a7f2e571d0e
Bruno Croci <crocidb@gmail.com>
2020-01-11 22:19:10 +0000

    added some animation to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,12 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using DG.Tweening;
+
 namespace Game.UI
 {
     public class MainMenu : MonoBehaviour
     {
         [Header("Panels")]
-        public GameObject m_TitlePanel;
-        public GameObject m_SelectionPanel;
+        public RectTransform m_TitlePanel;
+        public RectTransform m_SelectionPanel;
+
+        private Canvas m_Canvas;
+
+        private void Awake() 
+        {
+            m_Canvas = GetComponent<Canvas>();
+
+            m_TitlePanel.gameObject.SetActive(true);   
+            m_SelectionPanel.gameObject.SetActive(true);
+        }
+
+        public void ShowLevelSelection()
+        {
+            m_TitlePanel.DOAnchorPos(m_TitlePanel.anchoredPosition + Vector2.left * 800.0f, .2f);
+            m_SelectionPanel.DOAnchorPos(m_SelectionPanel.anchoredPosition + Vector2.left * 800.0f, .2f);
+        }
+
+        public void ShowMainMenu()
+        {
+            m_TitlePanel.DOAnchorPos(m_TitlePanel.anchoredPosition + Vector2.right * 800.0f, .2f);
+            m_SelectionPanel.DOAnchorPos(m_SelectionPanel.anchoredPosition + Vector2.right * 800.0f, .2f);
+        }
     }
 }
