92edc6da1acbfa64dee793d49a1d5216324373eb
Bruno Croci <crocidb@gmail.com>
2020-01-11 18:04:59 +0000

    very simple circuit generator

--- Assets/Scripts/Map/CircuitGenerator.cs
+++ Assets/Scripts/Map/CircuitGenerator.cs
@@ -3,39 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 #if UNITY_EDITOR
+using System.Linq;
 using UnityEditor;
 #endif
 
-[RequireComponent(typeof(LineRenderer))]
 public class CircuitGenerator : MonoBehaviour
 {
-    private LineRenderer m_CircuitLine;
+    private MeshFilter m_CircuitMesh;
 
     private void Awake() 
     {
-        m_CircuitLine = GetComponent<LineRenderer>();    
+        m_CircuitMesh = GetComponent<MeshFilter>();    
     }
     
+#if UNITY_EDITOR
     public void Generate()
     {
-        var positions = new List<Vector3>();
+        if (m_CircuitMesh == null)
+            m_CircuitMesh = GetComponent<MeshFilter>();
 
-        foreach (Transform pos in transform)
-        {
-            positions.Add(pos.position);
-            Debug.Log("Got " + positions);
-        }
-
-        positions.Add(transform.GetChild(0).position);
-
-        if (m_CircuitLine == null)
-            m_CircuitLine = GetComponent<LineRenderer>();  
-
-        m_CircuitLine.positionCount = positions.Count;
-        m_CircuitLine.SetPositions(positions.ToArray());
+        var mesh = DrawMesh();
+        m_CircuitMesh.sharedMesh = mesh;
     }
 
-#if UNITY_EDITOR
+    private Mesh DrawMesh()
+     {
+        var points = transform.Cast<Transform>().ToList();
+
+        var width = 8.0f;
+
+        Mesh mesh = m_CircuitMesh.sharedMesh;
+        if (mesh == null) mesh = new Mesh();
+        mesh.Clear();
+
+        // Generate mesh
+        var vertices = new List<Vector3>();
+        var normals = new List<Vector3>();
+        var uv = new List<Vector2>();
+        var triangles = new List<int>();
+
+        Transform lastPoint = points[points.Count - 1];
+        Vector3 lastLeft = Vector3.Cross((points[0].position - lastPoint.position).normalized, Vector3.up);;
+        int count = 0;
+        foreach (var pos in points)
+        {
+            var left = Vector3.Cross((pos.position - lastPoint.position).normalized, Vector3.up);
+
+            vertices.Add(lastPoint.position + lastLeft * width);
+            vertices.Add(lastPoint.position - lastLeft * width);
+            vertices.Add(pos.position + left * width);
+            vertices.Add(pos.position - left * width);
+
+            normals.Add(-Vector3.up);
+            normals.Add(-Vector3.up);
+            normals.Add(-Vector3.up);
+            normals.Add(-Vector3.up);
+
+            uv.Add(new Vector3(0.0f, 0.0f));
+            uv.Add(new Vector3(1.0f, 0.0f));
+            uv.Add(new Vector3(0.0f, 1.0f));
+            uv.Add(new Vector3(1.0f, 1.0f));
+
+            triangles.AddRange(new int[]{ 
+                count,      count + 2,  count + 1,
+                count + 2,  count + 3,  count + 1  });
+
+            count += 4;
+            lastPoint = pos;
+            lastLeft = left;
+        }
+
+        Debug.Log(vertices.Count);
+        Debug.Log(triangles.Count);
+
+        mesh.vertices = vertices.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = uv.ToArray();
+        mesh.triangles = triangles.ToArray();
+
+        mesh.RecalculateNormals();
+
+        return mesh;
+     }
+
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
