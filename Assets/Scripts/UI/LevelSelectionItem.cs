da391fe45f9a9bd91a851fbb0dd0c8a2953de365
Bruno Croci <crocidb@gmail.com>
2020-01-12 21:08:41 +0000

    improved overall UI

--- Assets/Scripts/UI/LevelSelectionItem.cs
+++ Assets/Scripts/UI/LevelSelectionItem.cs
@@ -11,7 +11,7 @@ namespace Game.UI
     {
         public Text m_Title;
         public Text m_Difficulty;
-        public Text m_UserStars;
+        public Image[] m_UserStars;
         public Text m_UserTime;
 
         [HideInInspector]
@@ -22,9 +22,17 @@ namespace Game.UI
             m_LevelDescriptor = levelDescriptor;
 
             m_Title.text = m_LevelDescriptor.m_Title;
-            m_Difficulty.text = m_LevelDescriptor.m_Title;
-            m_UserStars.text = "Stars";
-            m_UserTime.text = "UserTime";
+            m_Difficulty.text = m_LevelDescriptor.m_Difficulty.ToString();
+            m_UserTime.text = "---";
+
+            int stars = 2;
+            for (int i = 0; i < stars; i++)
+            {
+                var s = m_UserStars[i];
+                var color = s.color;
+                color.a = 1.0f;
+                s.color = color;
+            }
 
             GetComponent<Button>().onClick.AddListener(() => GameManager.Instance.LoadLevel(levelDescriptor));
         }
